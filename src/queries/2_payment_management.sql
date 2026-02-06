0b8762b20edd33310717dd7182bb7beba55d2b7c
farrukhthestudent <farrukh.ahmad@ada.ac.uk>
2026-02-06 13:01:19 +0000

    completed excersise 2

--- src/queries/2_payment_management.sql
+++ src/queries/2_payment_management.sql
@@ -2,10 +2,33 @@
 .mode column
 
 -- 2.1 
-
+/* Did not specify payment_date because schema has a default which selects current datetime
+when none is provided */
+INSERT INTO payments (
+    member_id,
+    amount,
+    payment_method,
+    payment_type
+) VALUES
+    (11,  50.00,  'Credit Card'   ,'Monthly membership fee'); 
 
 -- 2.2 
-
+SELECT
+  strftime('%Y-%m', payment_date) AS month,
+  SUM(amount) AS total_revenue
+FROM payments
+WHERE payment_type = 'Monthly membership fee'
+  AND payment_date >= '2024-11-01' AND payment_date < '2025-03-01'
+GROUP BY month
+ORDER BY month;
 
 -- 2.3 
+SELECT
+    payment_id,
+    amount,
+    payment_date,
+    payment_method
+FROM payments
+WHERE payment_type = 'Day pass';
+
 
