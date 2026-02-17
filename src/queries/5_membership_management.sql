5aa56431fe76d5179355edd7965254479e84d6d1
farrukhthestudent <farrukh.ahmad@ada.ac.uk>
2026-02-17 12:26:57 +0000

    completed exercise 5

--- src/queries/5_membership_management.sql
+++ src/queries/5_membership_management.sql
@@ -2,10 +2,46 @@
 .mode column
 
 -- 5.1 
-
-
--- 5.2 
-
+SELECT
+    members.member_id,
+    members.first_name,
+    members.last_name,
+    memberships.type AS membership_type,
+    memberships.start_date AS join_date
+FROM
+    memberships
+    JOIN members ON members.member_id = memberships.member_id
+WHERE
+    status = 'Active'
+    -- 5.2 
+SELECT
+    memberships.type AS membership_type,
+    ROUND(
+        AVG(
+            (
+                julianday(attendance.check_out_time) - julianday(attendance.check_in_time)
+            ) * 24 * 60
+        ),
+        1
+    ) AS avg_visit_duration_minutes
+FROM
+    attendance
+    JOIN memberships ON memberships.member_id = attendance.member_id
+WHERE
+    attendance.check_out_time IS NOT NULL
+GROUP BY
+    memberships.type;
 
 -- 5.3 
-
+SELECT
+    members.member_id,
+    members.first_name,
+    members.last_name,
+    members.email,
+    memberships.end_date
+FROM
+    memberships
+    JOIN members ON members.member_id = memberships.member_id
+WHERE
+    memberships.end_date >= '2025-01-01'
+    AND memberships.end_date < '2026-01-01'
